e15ffb771fc794c61c918de36b29479ba5caaa6e
linyangjiang39-debug <linyangjiang39@gmail.com>
2026-06-14 21:37:29 +0800

    更新员工数据 - 2026/6/14 21:37:28

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 // 自动生成 - SD相册员工数据
 const users = [
   {
-    jobNo: "012",
-    wechat: "",
-    phone: "",
-    photo: "./images/012.jpg"
-  },
-  {
     jobNo: "369",
     wechat: "",
     phone: "",
